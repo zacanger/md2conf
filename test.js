e6234174d8eb10cd23600ebc3367e0dd3c72d231
zacanger <zac@zacanger.com>
2018-10-03 20:37:42 -0600

    fix tests

--- test.js
+++ test.js
@@ -1,49 +1,55 @@
-/* eslint-disable */
-var md2conflu = require('./')
-var assert = require('assert')
+const md2conf = require('./')
+const tape = require('tape')
 
-var pairs = [
-  ['# h1', 'h1. h1\n\n'],
-  ['head1\n===', 'h1. head1\n\n'],
-  ['###  h3', 'h3. h3\n\n'],
-  ['- item\n  - nested', '\n* item\n** nested\n\n']
+const pairs = [
+  [ '# h1', 'h1. h1\n\n' ],
+  [ 'head1\n===', 'h1. head1\n\n' ],
+  [ '###  h3', 'h3. h3\n\n' ],
+  [ '- item\n  - nested', '\n* item\n** nested\n\n' ]
 ]
 
-test('basic', function () {
-  pairs.forEach(function (arr, i) {
-    expect(md2conflu(arr[0])).toBe(arr[1])
+tape.test('basic', (t) => {
+  pairs.forEach((arr) => {
+    t.equal(md2conf(arr[0]), arr[1])
   })
+  t.end()
 })
 
-test('header 1', function () {
-  expect(md2conflu('# Hello')).toBe('h1. Hello\n\n')
+tape.test('header 1', (t) => {
+  t.equal(md2conf('# Hello'), 'h1. Hello\n\n')
+  t.end()
 })
 
-test('heading 2', function () {
-  expect(md2conflu('## Hello')).toBe('h2. Hello\n\n')
+tape.test('heading 2', (t) => {
+  t.equal(md2conf('## Hello'), 'h2. Hello\n\n')
+  t.end()
 })
 
-test('heading 3', function () {
-  expect(md2conflu('### Hello')).toBe('h3. Hello\n\n')
+tape.test('heading 3', (t) => {
+  t.equal(md2conf('### Hello'), 'h3. Hello\n\n')
+  t.end()
 })
 
-test('heading 4', function () {
-  expect(md2conflu('#### Hello')).toBe('h4. Hello\n\n')
+tape.test('heading 4', (t) => {
+  t.equal(md2conf('#### Hello'), 'h4. Hello\n\n')
+  t.end()
 })
 
-describe('unordered list', function () {
-  test('basic', function () {
-    var list = ''.concat(
+tape.test('unordered list', (t) => {
+  t.test('basic', (t) => {
+    const list = ''.concat(
       '- this\n',
       '- is\n',
       '- a\n',
       '- list\n'
     )
-    var confluence_list = list.replace(/-/g, '*') + '\n'
-    expect(md2conflu(list)).toBe(confluence_list)
+    const confluenceList = '\n' + list.replace(/-/g, '*') + '\n'
+    t.equal(md2conf(list), confluenceList)
+    t.end()
   })
-  test('nested', function () {
-    var markdownList = ''.concat(
+
+  t.test('nested', (t) => {
+    const markdownList = ''.concat(
       '- this\n',
       '- is\n',
       '  - nested\n',
@@ -52,7 +58,8 @@ describe('unordered list', function () {
       '  - nested\n',
       '- list\n'
     )
-    var confluence_list = ''.concat(
+
+    const confluenceList = '\n'.concat(
       '* this\n',
       '* is\n',
       '** nested\n',
@@ -61,23 +68,28 @@ describe('unordered list', function () {
       '** nested\n',
       '* list\n\n'
     )
-    expect(md2conflu(markdownList)).toBe(confluence_list)
+    t.equal(md2conf(markdownList), confluenceList)
+    t.end()
   })
 })
 
-describe('ordered list', function () {
-  test('basic', function () {
-    var list = ''.concat(
+// eslint-disable-next-line max-lines-per-function
+tape.test('ordered list', (t) => {
+  t.test('basic', (t) => {
+    const list = ''.concat(
       '1. this\n',
       '1. is\n',
       '1. a\n',
       '1. list\n'
     )
-    var confluence_list = list.replace(/1\./g, '#') + '\n'
-    expect(md2conflu(list)).toBe(confluence_list)
+
+    const confluenceList = '\n' + list.replace(/1\./g, '#') + '\n'
+    t.equal(md2conf(list), confluenceList)
+    t.end()
   })
-  test('nested', function () {
-    var markdownList = ''.concat(
+
+  t.test('nested', (t) => {
+    const markdownList = ''.concat(
       '1. this\n',
       '1. is\n',
       '  1. nested\n',
@@ -86,7 +98,7 @@ describe('ordered list', function () {
       '  1. nested\n',
       '1. list\n'
     )
-    var confluence_list = ''.concat(
+    const confluenceList = '\n'.concat(
       '# this\n',
       '# is\n',
       '## nested\n',
@@ -95,10 +107,12 @@ describe('ordered list', function () {
       '## nested\n',
       '# list\n\n'
     )
-    expect(md2conflu(markdownList)).toBe(confluence_list)
+    t.equal(md2conf(markdownList), confluenceList)
+    t.end()
   })
-  test('nested mixed', function () {
-    var markdownList = ''.concat(
+
+  t.test('nested mixed', (t) => {
+    const markdownList = ''.concat(
       '1. this\n',
       '1. is\n',
       '  - nested\n',
@@ -107,7 +121,7 @@ describe('ordered list', function () {
       '  - nested\n',
       '1. list\n'
     )
-    var confluence_list = ''.concat(
+    const confluenceList = '\n'.concat(
       '# this\n',
       '# is\n',
       '#* nested\n',
@@ -116,71 +130,61 @@ describe('ordered list', function () {
       '#* nested\n',
       '# list\n\n'
     )
-    expect(md2conflu(markdownList)).toBe(confluence_list)
+    t.equal(md2conf(markdownList), confluenceList)
+    t.end()
   })
 })
 
-test('strong text', function () {
-  expect(md2conflu('**strong**')).toBe('*strong*\n\n')
+tape.test('strong text', (t) => {
+  t.equal(md2conf('**strong**'), '*strong*\n\n')
+  t.end()
 })
 
-test('italics text', function () {
-  expect(md2conflu('*some text here*')).toBe('_some text here_\n\n')
+tape.test('italics text', (t) => {
+  t.equal(md2conf('*some text here*'), '_some text here_\n\n')
+  t.end()
 })
 
-test('inline code', function () {
-  expect(md2conflu('`hello world`')).toBe('{{hello world}}\n\n')
+tape.test('inline code', (t) => {
+  t.equal(md2conf('`hello world`'), ' *{{hello world}}* \n\n')
+  t.end()
 })
 
-test('block of code', function () {
-  code = '    this is code\n'
-  expect(md2conflu(code)).toBe('{code:language=none|borderStyle=solid|theme=RDark|linenumbers=true|collapse=false}\nthis is code\n{code}\n\n')
+tape.test('block of code', (t) => {
+  const code = '    this is code\n'
+  t.equal(md2conf(code), '{code:language=none|borderStyle=solid|theme=RDark|linenumbers=true|collapse=false}\nthis is code\n{code}\n\n')
+  t.end()
 })
 
-test('strikethrough', function () {
-  expect(md2conflu('~~strikethrough text~~')).toBe('-strikethrough text-\n\n')
+tape.test('strikethrough', (t) => {
+  t.equal(md2conf('~~strikethrough text~~'), '-strikethrough text-\n\n')
+  t.end()
 })
 
-test('quote', function () {
-  expect(md2conflu('> this is a quote')).toBe('{quote}this is a quote\n\n{quote}')
+tape.test('quote', (t) => {
+  t.equal(md2conf('> this is a quote'), '{quote}this is a quote\n\n{quote}')
+  t.end()
 })
 
-describe('hyperlink', function () {
-  test('with no text or title', function () {
-    expect(md2conflu('[](http://github.com)')).toBe('[http://github.com]\n\n')
-    expect(md2conflu('http://github.com')).toBe('[http://github.com|http://github.com]\n\n')
-  })
-  test('with text', function () {
-    expect(md2conflu('[github](http://github.com)')).toBe('[github|http://github.com]\n\n')
-  })
-  test('with title', function () {
-    expect(md2conflu('[](http://github.com "Github")')).toBe('[http://github.com|Github]\n\n')
-  })
-  test('with text and title', function () {
-    expect(md2conflu('[github](http://github.com "Github")')).toBe('[github|http://github.com|Github]\n\n')
-  })
+tape.test('hyperlink', (t) => {
+  t.equal(md2conf('[](http://github.com)'), '[http://github.com]\n\n')
+  t.equal(md2conf('http://github.com'), '[http://github.com|http://github.com]\n\n')
+  t.equal(md2conf('[github](http://github.com)'), '[github|http://github.com]\n\n')
+  t.equal(md2conf('[](http://github.com "Github")'), '[http://github.com|Github]\n\n')
+  t.equal(md2conf('[github](http://github.com "Github")'), '[github|http://github.com|Github]\n\n')
+  t.end()
 })
 
-describe('image link', function () {
-  test('without alt', function () {
-    expect(md2conflu('![](http://github.com/logo.png)')).toBe('!http://github.com/logo.png!\n\n')
-  })
-  test('with alt', function () {
-    expect(md2conflu('![logo](http://github.com/logo.png)')).toBe('!http://github.com/logo.png|alt=logo!\n\n')
-  })
+tape.test('image link', (t) => {
+  t.equal(md2conf('![](http://github.com/logo.png)'), '!http://github.com/logo.png!\n\n')
+  t.equal(md2conf('![logo](http://github.com/logo.png)'), '!http://github.com/logo.png|alt=logo!\n\n')
+  t.end()
 })
 
-describe('horizontal rule', function () {
-  test('* * *', function () {
-    expect(md2conflu('* * *')).toBe('----')
-  })
-  test('***', function () {
-    expect(md2conflu('***')).toBe('----')
-  })
-  test('*****', function () {
-    expect(md2conflu('*****')).toBe('----')
-  })
-  test('---------------------------------------', function () {
-    expect(md2conflu('---------------------------------------')).toBe('----')
-  })
+tape.test('horizontal rule', (t) => {
+  t.equal(md2conf('* * *'), '----')
+  t.equal(md2conf('***'), '----')
+  t.equal(md2conf('*****'), '----')
+  t.equal(md2conf('---------------------------------------'), '----')
+  t.end()
 })
